ca53eab305df4014b12bc41cf2697a217e036f02
dudux7 <eduardogerebatista556@gmail.com>
2024-08-28 17:06:58 -0300

    teste php

--- verificando.php
+++ verificando.php
@@ -1,21 +1,32 @@
 <?php
-    $a = 10;
-
-
-
-
-
-
-
-
-
-
-
-
+    $a = -456;
+    $b = false;
+    $c = "0.87";
+    $d = 1.67;
+    $e = "Casa 8";
+    $f = 0;
+    $g = 1000;
+    $h = true;
+    $i = "-900";
+    $j = 3;
+    $k = 0.0;
+    $l = -3.1415; 
+    $m = 32150;
+    $n = 128288; 
+    $o = "false";
 
 
     if (is_int($a)) {
         echo "é um inteiro";
+
+        elseif (is_float()) {
+            echo "é decimal";
+
+            elseif (is_string()) {
+                echo "é uma string"
+                
+            }
+        }
     }
 
 
