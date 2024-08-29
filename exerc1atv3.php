ffa4f919731c0582d173422197ea7f9ec918a540
dudux7 <eduardogerebatista556@gmail.com>
2024-08-29 16:02:14 -0300

    alterando nome de arquivos

---
+++ exerc1atv3.php
@@ -0,0 +1,13 @@
+<?php
+    echo "digite o primeiro numero";
+    $a = readline();
+     echo "digite o segundo numero";
+    $b = readline();
+    echo "digite o terceiro numero ";
+    $c = readline();
+     echo "digite o quarto numero";
+    $d = readline();
+
+    $media = ($a+$b+$c+$d) /4;
+    echo = "A media dos quatro numeros somados é: $media";
+    
