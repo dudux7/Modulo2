d7a4962d25bc16810a7e24896bbc8679871fd5f2
dudux7 <eduardogerebatista556@gmail.com>
2024-08-30 16:45:19 -0300

    upando atv3

--- exerc1atv3.php
+++ exerc1atv3.php
@@ -1,13 +1,31 @@
 <?php
     echo "digite o primeiro numero";
-    $a = readline();
-     echo "digite o segundo numero";
-    $b = readline();
+        $a = readline();
+    echo "digite o segundo numero";
+        $b = readline();
     echo "digite o terceiro numero ";
-    $c = readline();
-     echo "digite o quarto numero";
-    $d = readline();
-    $media = ($a+$b+$c+$d) /4;
-    echo "A media dos quatro numeros somados é: $media";
-    
+        $c = readline();
+    echo "digite o quarto numero";
+        $d = readline();
+
+    $peso1 = 1;
+    $peso2 = 2;
+    $peso3 = 3;
+    $peso4 = 4;
+
+    $mult1 = $a * $peso1;
+    $mult2 = $b * $peso2;
+    $mult3 = $c * $peso3;
+    $mult4 = $d * $peso4;
+
+    $somanum = $mult1 + $mult2 + $mult3 + $mult4;
+
+    $somapeso = $peso1 + $peso2 + $peso3 + $peso4;
+
+    $media = $somanum / $somapeso;
+
+    echo "o resultado é $media"
+
+
+
 ?>
