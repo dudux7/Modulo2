a24301f8a0e5e0564ed8dfdae77761411ecce2ec
dudux7 <eduardogerebatista556@gmail.com>
2024-09-05 03:59:05 -0300

    function e operações

--- projetomodulo.php
+++ projetomodulo.php
@@ -9,6 +9,52 @@ $adm = ['admin' => 'admin'];
         return ($adm[$usuario]) && $adm[$usuario] === $senha;
     }
     
+     /*função de operações*/
+     function operações() {
+        global $total;
+        system('cls');
+        echo "Usuário $usuario logado\n";
+        
+        echo "Valor total arrecadado: R$ $total com $i vendas\n";
+       
+        echo "1 - Cadastrar novo usuário\n";
+        echo "2 - Verificar login\n";
+        echo "3 - Deslogar\n";
+        
+        echo "4 - Vender\n";
+      
+        echo "5 - Sair do Programa\n";
+    }
+    
+        
+        /* funções de venda */
+        function venda() {
+        global $total, $i;
+        system('cls');
+        echo "Nome do produto: ";
+        $produto = readline();
+        echo "Valor do(a) $produto: ";
+        $valor = (float)readline();
+        $total += $valor;
+        $i = $i++;
+    }
+        
+        
+        
+        /* função para novos usuários */
+            function cadastrar() {
+            system('cls');
+            global $adm;
+            
+            echo "Digite o nome do novo usuário: ";
+            $usuario = readline();
+            
+            echo "Digite a senha do novo usuário: ";
+            $senha = readline();
+            
+            $adm[$usuario] = $senha;
+            echo "Sucesso\n";
+        }
     
     /*logar*/
     
@@ -25,6 +71,27 @@ while (true) {
             echo "Login incorreto\n";
         }
         
+        operações();
+        echo"Digite a operação desejada: ";
+        $operação = readline();
+        switch ($operação) {
+            case 1:
+                cadastrar();
+                break;
+            case 2:
+                vereficar();
+                break;
+            case 3:
+                deslogar();
+                break;
+            case 4:
+                venda();
+                break;
+            case 5:
+                break 2; 
+            default:
+                echo "Opção inválida\n";
+        }
     }
 
 
